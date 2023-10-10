bc4d5c748ce286af2c1ab3f579b9747c695bc614
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-10-10 13:07:43 +0000

    fbazaar api,edited functions for adding to cart and favorites

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Cart
 from .serializers import CartSerializer
 from django.http import Http404
--- favorites/views.py
+++ favorites/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Favorites
 from .serializers import FavoritesSerializer
 from django.http import Http404

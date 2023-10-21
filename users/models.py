92ca3328d4981661905a9ae3f19d42f6b8956fad
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-10-21 09:30:24 +0000

    fbazaar api,getting admin user,added full name to user model

--- users/models.py
+++ users/models.py
@@ -15,11 +15,11 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, max_length=255)
     phone = models.CharField(max_length=15, unique=True, help_text="please format should be +233")
     user_type = models.CharField(max_length=80,choices=USER_TYPES,default="Customer")
-    full_name = models.CharField(max_length=100, unique=True, default="")
+    name = models.CharField(max_length=100, unique=True, default="")
     user_blocked = models.BooleanField(default=False)
     agreed_to_supplied = models.BooleanField(default=False)
 
-    REQUIRED_FIELDS = ['username', 'phone','user_type','full_name']
+    REQUIRED_FIELDS = ['username', 'phone','user_type','name']
     USERNAME_FIELD = 'email'
 
     def get_username(self):

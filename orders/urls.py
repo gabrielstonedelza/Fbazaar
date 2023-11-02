7e7d07931750d8a4a71c7ab523e07ecdb45c56dc
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-11-02 13:27:43 +0000

    fbazaar api,whole app restarted and remodified

--- orders/urls.py
+++ orders/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns =[
     path("add_item_to_cart/<int:id>/",views.add_item_to_cart),
-    path("remove_item_from_cart/<int:id>/<int:order_item_id>/",views.remove_item_from_cart),
+    path("remove_item_from_cart/<int:id>/",views.remove_item_from_cart),
     path("increase_item_quantity/<int:id>/<int:order_item_id>/",views.increase_item_quantity),
     path("decrease_item_quantity/<int:id>/<int:order_item_id>/",views.decrease_item_quantity),
     path("get_not_ordered_items/",views.get_not_ordered_items),

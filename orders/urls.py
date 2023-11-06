3eb7e30a3fac89fcc4aabe08d612865a55bf47bd
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-11-06 19:57:34 +0000

    fbazaar api,added total order price to order model

--- orders/urls.py
+++ orders/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns =[
     path("item_to_cart/<int:id>/",views.item_to_cart),
-    path("item_check_out/<str:pm>/<str:dm>/<str:drop_loc_lat>/<str:drop_off_lng>/<str:unique_code>/",views.item_check_out),
+    path("item_check_out/<str:pm>/<str:dm>/<str:drop_loc_lat>/<str:drop_off_lng>/<str:unique_code>/<str:total_price>/",views.item_check_out),
     # path("add_item_to_cart/<int:id>/",views.add_item_to_cart),
     path("remove_item_from_cart/<int:id>/",views.remove_item_from_cart),
     path("increase_item_quantity/<int:id>/",views.increase_item_quantity),

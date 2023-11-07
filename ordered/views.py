4bd04394eb4c48c83d1a65d403f7351332c350af
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-11-07 03:16:06 +0000

    fbazaar api,added ordered model

--- ordered/views.py
+++ ordered/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import viewsets, permissions, generics, status
 from rest_framework.response import Response
+from order.models import Order
 
 
 @api_view(['GET'])
